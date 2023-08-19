3cc33305108e5ba0b75897a8116233e2fe530772
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2023-08-19 13:24:57 +0600

    FT-FE-BackEnd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a5c578dd9d9b1c20"; }
+/******/ 		__webpack_require__.h = function() { return "ed1cc11d1dedcd88"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/component/layout.js
+++ pages/component/layout.js
@@ -23,14 +23,14 @@ export default function MyLayout(props) {
               </li>
             </ul>
             <ul className="flex p-4 space-x-4">
-              {/* <li>
-                <Link href="auth/signin" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Sign In</Link>
-              </li> */}
+              <li>
+                <Link href="/auth/signin" className="text-white bg-gray-400 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign In</Link>
+              </li>
               <li>
                 <Link href="/auth/registration" className="text-white bg-gray-400 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Registration</Link>
               </li>
             </ul>
-            <Session/>
+            {/* <Session/> */}
           </div>
         </div>
       </nav>
